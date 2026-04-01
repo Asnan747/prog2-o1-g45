a0ebe683d7bcb53c37dc6ea54e443d870c2227c2
unknown <asnan747gmail.com>
2026-04-01 21:10:12 +0200

    Ändringar på recording klassen

--- src/main/java/se/su/ovning1/Recording.java
+++ src/main/java/se/su/ovning1/Recording.java
@@ -1,4 +1,36 @@
 package se.su.ovning1;
 
-public class Recording {
+public abstract class Recording {
+
+    //Variabler
+    private String artist;
+    private int year;
+    private int condition;
+    private double price;
+
+    //Konstruktor
+    public Recording(String artist, int year, int condition, double price) {
+        this.artist = artist;
+        this.year = year;
+        this.condition = condition;
+        this.price = price;
+    }
+
+    //Metoder
+    public String getArtist() {
+        return artist;
+    }
+
+    public int getCondition() {
+        return condition;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getYear() {
+        return year;
+    }
+
 }
