6c599dcc9c27c87d7acbcf2ff96ee2798cd05c89
hared <abdullahi.hared.work@gmail.com>
2026-04-02 11:09:24 +0200

    Skapa konstruktör som tar in valfrit Item

--- src/main/java/se/su/ovning1/Order.java
+++ src/main/java/se/su/ovning1/Order.java
@@ -1,4 +1,13 @@
 package se.su.ovning1;
 
+import java.util.ArrayList;
+
 public class Order {
+    ArrayList<Item[]> items = new ArrayList<>();
+    // ta in valfrit antal Items och lägga till i listan.
+    public Order(Item... item) {
+        items.add(item);
+    }
+
+
 }
