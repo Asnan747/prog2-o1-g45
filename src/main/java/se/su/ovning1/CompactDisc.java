4464cb099ed89b0942c3eb463cc171718a9fb693
unknown <asnan747gmail.com>
2026-04-02 15:05:52 +0200

    Ändring på CompactDisc

--- src/main/java/se/su/ovning1/CompactDisc.java
+++ src/main/java/se/su/ovning1/CompactDisc.java
@@ -1,4 +1,15 @@
 package se.su.ovning1;
 
-public class CompactDisc {
+public class CompactDisc extends Recording {
+
+
+
+    //KOnstruktor
+    public CompactDisc(String artist, int year, int condition, double price) {
+        super(artist, year, condition, price);
+    }
+
+    public String getType() {
+        return "LP-skiva";
+    }
 }
